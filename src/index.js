f74098c81ef9083e1e6bed9b8a2abd0f3e9bed24
Sarah Thompson <sethompson@upei.ca>
2019-08-09 15:31:52 -0300

    added the handleClick function

--- src/index.js
+++ src/index.js
@@ -24,6 +24,13 @@ class Square extends React.Component {
         }
     }
 
+    handleClick(i)
+    {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
       return (
         <Square 
